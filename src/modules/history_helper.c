5b9887018e65fdf03af44f4b5aa0e4b30d530e2a
medvecky <alexey.medvecky@gmail.com>
2024-06-23 19:53:54 +0300

    implement_history_entry_editing: * fix delete symbols in history entry behavior

--- src/modules/history_helper.c
+++ src/modules/history_helper.c
@@ -108,10 +108,10 @@ static void editHistoryEntry( char * entryString )
 static void cursorHandler( int cursorYPosition, char * entryString )
 {
     char currentKey = cgetc();
-    size_t entryLength = strlen( entryString );
+    size_t entryLength = strlen( entryString );   
     int currentCursorPosition = entryLength - 2;
 
-     if ( entryLength > 41) cursorYPosition--;
+    if ( entryLength > 41) cursorYPosition--;
 
     while ( currentKey != 0x0a )
     {
@@ -142,7 +142,12 @@ static void cursorHandler( int cursorYPosition, char * entryString )
         gotoxy( 0, cursorYPosition );
         rewriteString( entryString, currentCursorPosition );
         currentKey = cgetc();
+        entryLength = strlen( entryString ); 
     }
+
+     gotoxy( 0, cursorYPosition );
+     printf( "%s  ", entryString );
+
 }
 
 static void rewriteString( char * entryString, int currentCursorPosition )
@@ -161,21 +166,23 @@ static void rewriteString( char * entryString, int currentCursorPosition )
             printf( "%c", entryString[ index ] );
         }
     }
-    printf( "%s", " " );
+    
+    if ( entryLength <= 0 ) 
+    {
+        printf( "%s","]     " );
+    }
+    else
+    {
+         printf( "%s", "     "  );
+    }
 }
 
-// TODO: DEBUG this function 
-//  * display the string correctly
-//  * check first cursor position
-//  * rewite string after deleting a character
-//  * create debug ooportunity for the function  
 static void handleBackspace( char * entryString, int * cursorYPosition, int * currentCursorPosition )
 {
     size_t entryLength = strlen( entryString );
     if ( *currentCursorPosition < entryLength ) 
     {
         memmove( &entryString[ *currentCursorPosition ], &entryString[ *currentCursorPosition + 1], entryLength - *currentCursorPosition );
-        entryLength--;
-        ( *currentCursorPosition )--;
+        if ( *currentCursorPosition > 0 ) ( *currentCursorPosition )--;
     }
 }
